83fc02869ba3cadb46057ee2c2621b7744b9df5a
CppKotlin <cscppkotlin@gmail.com>
2024-01-28 21:10:03 +0300

    thread th(DoWorK); th.join();

--- Multithreading/Multithreading.cpp
+++ Multithreading/Multithreading.cpp
@@ -1,4 +1,6 @@
 ﻿#include<iostream>
+#include<thread>
+#include<chrono>
 
 using namespace std;
 
@@ -6,12 +8,37 @@ using namespace std;
 				//Thread 
 			
 
+void DoWorK() 
+{
+	cout << "Start DoWorK()" << endl;
+	for (int i = 0; i < 10; ++i)
+	{
+
+		cout << "ID thread = " << this_thread::get_id() << "\t void DoWorK(){} " << i << endl;
+		this_thread::sleep_for(chrono::milliseconds(1000));
+	}
+
+	cout << "END DoWorK()" << endl;
+
+}
+
 int main()
 {
 	setlocale(LC_ALL, "ru");
+	thread th(DoWorK);
+	thread th2(DoWorK);
 
+	cout << "Start int main()" << endl;
+	for (int i=0;i<10; ++i)
+	{
 
+		cout << "ID thread = " << this_thread::get_id() << "\t int main(){} " << i << endl;
+		this_thread::sleep_for(chrono::milliseconds(500));
+	}
 
-
+	cout << "END int main()" << endl;
+	
+	th.join();
+	th2.join();
 	return 0;
 }
