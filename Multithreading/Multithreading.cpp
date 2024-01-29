b1d64460feec5a9e9ea610878c36afb167794ed5
CppKotlin <cscppkotlin@gmail.com>
2024-01-29 21:58:21 +0300

    //Поток с параметрами

--- Multithreading/Multithreading.cpp
+++ Multithreading/Multithreading.cpp
@@ -5,40 +5,36 @@
 using namespace std;
 
 				//Многозадачность и многопоточность
-				//Thread 
+				//Поток с параметрами
 			
 
-void DoWorK() 
+void DoWorK(int a,int b,string msg)
 {
-	cout << "Start DoWorK()" << endl;
-	for (int i = 0; i < 10; ++i)
-	{
-
-		cout << "ID thread = " << this_thread::get_id() << "\t void DoWorK(){} " << i << endl;
-		this_thread::sleep_for(chrono::milliseconds(1000));
-	}
-
-	cout << "END DoWorK()" << endl;
+	cout << "------------\tStart DoWorK()\t------------" << endl;
+	this_thread::sleep_for(chrono::milliseconds(2000));
+	cout << "\t DoWork = " << a + b <<"\t" << msg << endl;
+	this_thread::sleep_for(chrono::milliseconds(2000));
+	cout << "------------\tEND DoWorK()\t------------\t" << endl;
 
 }
 
 int main()
 {
 	setlocale(LC_ALL, "ru");
-	thread th(DoWorK);
-	thread th2(DoWorK);
+	thread th(DoWorK,2, 4, "MSG *Hi bro*");
+	
 
-	cout << "Start int main()" << endl;
-	for (int i=0;i<10; ++i)
+	
+	for (rsize_t i=0;true; ++i)
 	{
 
 		cout << "ID thread = " << this_thread::get_id() << "\t int main(){} " << i << endl;
 		this_thread::sleep_for(chrono::milliseconds(500));
 	}
 
-	cout << "END int main()" << endl;
+
 	
-	th.join();
-	th2.join();
+	//th.join();
+	
 	return 0;
 }
