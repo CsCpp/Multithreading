109e672e5438ab9588b0f2544dc68ebe65af2e18
CppKotlin <cscppkotlin@gmail.com>
2024-01-31 22:24:12 +0300

    //Многозадачность и многопоточность // лямбда функции

--- Multithreading/Multithreading.cpp
+++ Multithreading/Multithreading.cpp
@@ -5,19 +5,18 @@
 using namespace std;
 
 				//Многозадачность и многопоточность
-				// std::ref()
-				//получение результатов работы функции из потока 
+				// лямбда функции
 			
 
-void DoWorK(int &a,int &b)
+int Sum(int &a,int &b)
 {
-	cout << "------------\tStart DoWorK()\t------------" << endl;
-	this_thread::sleep_for(chrono::milliseconds(2000));
-	a =a + b;
-	cout << "\t DoWork = " << a <<"\t" << endl;
-	this_thread::sleep_for(chrono::milliseconds(2000));
-	cout << "------------\tEND DoWorK()\t------------\t" << endl;
-
+	cout << "------------\tStart Sum()\t------------" <<this_thread::get_id()<< endl;
+	this_thread::sleep_for(chrono::milliseconds(3000));
+	
+	cout << "\t Sum = " << a+b <<"\t" << endl;
+	this_thread::sleep_for(chrono::milliseconds(3000));
+	cout << "------------\tEND Sum()\t------------\t" << this_thread::get_id() << endl;
+	return a + b;
 }
 
 int main()
@@ -25,14 +24,18 @@ int main()
 	setlocale(LC_ALL, "ru");
 	int a = 2;
 	int b = 4;
-	//DoWorK(a, b);
-	thread th(DoWorK, std::ref(a), std::ref(b));
 	
-
+	int result;
+	 
+	thread th([&result,&a,&b]() {result = Sum(a, b); });
+	int result2;
+	auto f = [&result2, &a, &b]() {result2 = Sum(a, b); };
+	
+	thread th2(f);
 	
 	for (rsize_t i=0;i<10; ++i)
 	{
-
+		
 		cout << "ID thread = " << this_thread::get_id() << "\t int main(){} " << i << endl;
 		this_thread::sleep_for(chrono::milliseconds(500));
 	}
@@ -40,6 +43,8 @@ int main()
 
 	
 	th.join();
-	cout << a << endl;
+	th2.join();
+	cout << result << " - result " << endl;
+	cout << result2 << " - result2 " << endl;
 	return 0;
 }
