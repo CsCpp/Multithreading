5601a447b3a98edfdaa1ee628113326e4171c63e
CppKotlin <cscppkotlin@gmail.com>
2024-02-04 21:20:05 +0300

    //взаимная блокировка // DeadLock

--- Multithreading/Multithreading.cpp
+++ Multithreading/Multithreading.cpp
@@ -5,24 +5,27 @@
 
 using namespace std;
 
-//Lock_guard
-// синхронизация потоков				
-//Защита разделяемых данных
-								
-mutex mtx;
+//взаимная блокировка
+// DeadLock
 
-void Print(char ch)
+								
+mutex mtx1;
+mutex mtx2;
+
+void Print()
 {
-	
-	this_thread::sleep_for(chrono::milliseconds(2000));
+	mtx1.lock();
+	this_thread::sleep_for(chrono::milliseconds(1000));
+	mtx2.lock();
+	this_thread::sleep_for(chrono::milliseconds(200));
 	{
-		lock_guard<mutex> guard(mtx);
+		
 		for (int i = 0; i < 5; ++i)
 		{
 			for (int i = 0; i < 10; i++)
 			{
 
-				cout << ch;
+				cout << "*";
 
 				this_thread::sleep_for(chrono::milliseconds(20));
 
@@ -35,20 +38,53 @@ void Print(char ch)
 		cout << endl;
 	}
 	
-	this_thread::sleep_for(chrono::milliseconds(2000));
+	this_thread::sleep_for(chrono::milliseconds(200));
+	
+	mtx1.unlock();
+	mtx2.unlock();
 }
+void Print2()
+{
+	mtx2.lock();
+	this_thread::sleep_for(chrono::milliseconds(1000));
+	mtx1.lock();
+	this_thread::sleep_for(chrono::milliseconds(200));
+	{
+		
+		for (int i = 0; i < 5; ++i)
+		{
+			for (int i = 0; i < 10; i++)
+			{
 
+				cout << "#";
+
+				this_thread::sleep_for(chrono::milliseconds(20));
+
+			}
+
+			cout << endl;
+
+		}
+
+		cout << endl;
+	}
+
+	this_thread::sleep_for(chrono::milliseconds(200));
+
+	mtx1.unlock();
+	mtx2.unlock();
+}
 
 int main()
 {
 	setlocale(LC_ALL, "ru");
-	thread t1(Print, '*');
-	thread t2(Print, '#');
-	thread t3(Print, '&');
+	thread t1(Print);
+	thread t2(Print2);
+	
 	
 	t1.join();
 	t2.join();
-	t3.join();
+	
 
 	return 0;
 }
